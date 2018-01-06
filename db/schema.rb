19de5a99a48f2dc40620f9e18a60919037485b44
Reynaldo Esparza <reynaldoeg_esparza@hotmail.com>
2018-01-06 19:53:24 +0000

    Games Scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180106194122) do
+ActiveRecord::Schema.define(version: 20180106195147) do
+
+  create_table "games", force: :cascade do |t|
+    t.integer "user1_id"
+    t.integer "user2_id"
+    t.integer "user1_score"
+    t.integer "user2_score"
+    t.integer "winner"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
